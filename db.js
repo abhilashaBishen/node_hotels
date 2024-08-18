6ec361d6ae3a5e93c6c24cf9dacbed6663dbbf2b
abhilasha singh <abhilashasingh1391@gmail.com>
2024-08-18 18:16:47 +0530

    some changes env_file

--- db.js
+++ db.js
@@ -1,15 +1,23 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 //Define the MongoDb connection URL
-const mongoURL = 'mongodb://127.0.0.1:27017/hotels';
+//const mongoURL = 'mongodb://127.0.0.1:27017/hotels';
+
+// const mongoURL= process.env.MONGODB_URL_LOCAL;
+
+
+
+const mongoURL = process.env.MONGODB_URL
 
 //set a mongodb connection
-
 mongoose.connect(mongoURL,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
+//get the default connection
+//Mongoose maintain a default connection object representing the MongoDB connection.
 const db = mongoose.connection;
 
 db.on('connected',()=>{
--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 
 const db = require('./db');
+require('dotenv').config();
 
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());//it willstore in req.body
-
+const PORT = process.env.PORT || 3000;
 const personRoutes = require('./routes/personRoutes');
 
 app.use('/person',personRoutes);
@@ -17,10 +18,20 @@ const menuRoutes = require('./routes/menuRoutes')
 app.use('/menu',menuRoutes);
 
 app.get('/',function(req,res){
+
     res.send('welome to my hotel... How ican help you');
+    
 })
 
+app.get("/chefs", function(req,res){
+   
+    res.send('this is data about chef in our hotel');
+  
 
-app.listen(3000,()=>{
+})
+
+app.listen(PORT,()=>{
+ 
     console.log('server is running at http://localhost:3000')}
+ 
 );
